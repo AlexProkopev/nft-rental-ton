dbfef766d00384fa9acfbccc3dafbf043c52671d
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2024-07-03 18:32:31 +0200

    add

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -4,7 +4,6 @@ export const REGISTR_ROUTE = "/registr";
 export const CHANGE_ROUTE = "/change";
 export const TOKENOMICS_ROUTE = "/tokenomics";
 export const ROAD_ROUTE = "/road";
-export const PAPER_ROUTE = "/paper";
 export const CABINET_ROUTE = "/profile";
 export const REQUEST_ROUTE = "/create-request";
 export const CRYPTOTABLE_ROUTE = "/crypto-table";
