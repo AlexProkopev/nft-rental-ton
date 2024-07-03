6a15b180ecde01ec25bf2afdaafc3e239812c1b2
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2024-07-03 07:08:42 +0200

    add

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { persistor, store } from './redux/state/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/nft-rental-ton">
     <Provider store={store}>
     <PersistGate persistor={persistor}>
     <App />
